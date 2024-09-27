945106b504e96c81e8b752e61ff8d7d52357bb19
nikolaj1307 <nikolaj1307@live.dk>
2024-09-27 10:58:16 +0200

    ProductManagementPage
    Added ProductTable & PopUp window for edit details.

--- client/src/pages/ProductManagementPage.tsx
+++ client/src/pages/ProductManagementPage.tsx
@@ -1,10 +1,17 @@
 import Navbar from "../components/Navbar.tsx";
+import ProductTable from "../components/ProductTable.tsx";
+import { Toaster } from 'react-hot-toast';
+import {useInitializeData} from "../useInitializeData.ts";
 
 export default function ProductManagementPage() {
+
+    useInitializeData();
+
   return (
     <>
       <Navbar />
-      <div>ProductManagementPage</div>
+      <ProductTable />
+        <Toaster position={"top-right"}/>
     </>
   );
 }
