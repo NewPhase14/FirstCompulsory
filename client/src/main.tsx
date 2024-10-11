ecd682d5cee8d44ab3ada2da204b790be8410071
NewPhase14 <jepbad01@easv365.dk>
2024-10-11 11:55:25 +0200

    removed devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import React, { StrictMode } from "react";
-import "jotai-devtools/styles.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
@@ -9,8 +8,6 @@ import ProductCatalog from "./pages/ProductCatalog.tsx";
 import OrderHistoryPage from "./pages/OrderHistoryPage.tsx";
 import ProductManagementPage from "./pages/ProductManagementPage.tsx";
 import ShoppingCartPage from "./pages/ShoppingCart.tsx";
-import { DevTools } from "jotai-devtools";
-import "jotai-devtools/styles.css";
 import CustomerPage from "./pages/CustomerPage.tsx";
 
 const router = createBrowserRouter([
@@ -43,7 +40,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DevTools />
     <RouterProvider router={router} />
   </StrictMode>,
 );
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import Navbar from "../components/Navbar.tsx";
 
 export default function HomePage() {
-    return (
-        <>
-            <Navbar />
-            <div className="homepage-div flex items-center justify-center relative h-screen">
-                <div className="overlay"></div> {/* Add the overlay for background */}
-                <div className="text-content z-10"> {/* Use z-10 to ensure text is above overlay */}
-                    <h1 className="text-outline-white text-5xl mb-2">Welcome to Dunder Mifflin</h1>
-                    <p className="text-lg text-white">Your shop for paper products!</p>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="pt-20">
+        <div className="homepage-div flex items-center justify-center relative h-screen">
+          <div className="overlay"></div> {/* Add the overlay for background */}
+          <div className="text-content z-10">
+            {" "}
+            {/* Use z-10 to ensure text is above overlay */}
+            <h1 className="text-outline-white text-5xl mb-2">
+              Welcome to Dunder Mifflin
+            </h1>
+            <p className="text-lg text-white">Your shop for paper products!</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
